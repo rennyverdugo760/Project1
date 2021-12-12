0caea4c21f573837ee9190e5090f75bc09ef5b60
rennyverdugo760 <83443762+rennyverdugo760@users.noreply.github.com>
2021-12-12 00:14:56 -0500

    Delete Linux/Security_scripts directory

--- Linux/Security_scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tar -cvvWf /var/backup/home.tar /home
-mv /var/backup/home.tar /var/backup/home.10022021.tar
-ls -lah /var/backup > /var/backup/file_report.txt
-free -h > /var/backup/disk_report.txt
-
-
